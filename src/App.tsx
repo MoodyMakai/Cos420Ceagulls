c07b114c64e072f42b9108dc3d8eb4ec365128db
Tyler Reilly <reilly55@purdue.edu>
2025-03-14 10:44:37 -0400

    Added a background image, and made the table visible with that change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../src/assets/snake-image.png'
 
 
 function App() {
@@ -26,8 +27,12 @@ function App() {
       position: 'absolute',
       top: 0,
       left: 0,
-      width: '100%', // Optional: to make it span the entire width
-      height: 'auto' // Optional: or set a specific height
+      width: '100%',
+      height: '100vh', // Optional: adjust as needed',
+      backgroundImage: `url(${'../src/assets/snake-image.png'})`,
+      backgroundSize: 'cover', // Optional: adjust as needed
+      backgroundRepeat: 'no-repeat' // Optional
+
     }}>
           <h1 className='row' style={{ display: 'flex', justifyContent: 'center' }}> SUPER SNAKE</h1>
           <div className='row'>
@@ -40,7 +45,7 @@ function App() {
               <div className='col-9'>
                 <h1 hidden>Do nothing here</h1>
               </div>
-              <table id='myDIV' className='col-3 table' style={{ display: 'none' }}>
+              <table id='myDIV' className='col-3 table table-dark ' style={{ display: 'none' }}>
                     <tr>
                       <th>#</th>
                       <th>User Name</th>
