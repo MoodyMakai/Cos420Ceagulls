9782ffafbf60e7d3c039ff08903f1cb37b0358f7
MatthewTinker <matthew.tinkham@maine.edu>
2025-04-13 13:35:21 -0400

    Skin

---
+++ src/skin.ts
@@ -0,0 +1,17 @@
+/*
+interface Skin {
+    sprite: string, //This is a url to the sprite map in the folder
+    name: string, //This is the name of the sprite itself
+    id: number, //This is an ID for the skin
+}
+
+function CreateSkins(): void {
+
+
+}
+
+export function ChangeSkin(){
+
+
+
+}*/
